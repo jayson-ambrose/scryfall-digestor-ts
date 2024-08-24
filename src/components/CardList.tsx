16aa6e3eb2114917cdca3e3af4692abfd88b542a
jayson-ambrose <jayson.ambrose.87@gmail.com>
2024-08-24 08:22:41 -0600

    minor changes

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -3,7 +3,7 @@ import { ScrollArea } from "@/components/ui/scroll-area"
 export default function CardList({ children }: { children: React.ReactNode }) {
 
     return (
-        <ScrollArea className='border-4 border-slate-600 flex flex-col w-1/2 max-h-100vw h-screen px-2 ml-2'>
+        <ScrollArea className='border-4 border-slate-600 flex flex-col w-1/2 max-h-100vw h-screen px-2 ml-2 bg-white'>
             {children}
         </ScrollArea>
     )
--- src/components/CardListItem.tsx
+++ src/components/CardListItem.tsx
@@ -5,7 +5,7 @@ export default function CardListItem({ card, targetCard }: { card: any, targetCa
     }
 
     return (
-        <div onClick={() => handleClick()} className='flex rounded-xl border border-black my-1 ml-5 justify-between'>
+        <div onClick={() => handleClick()} className='flex rounded-xl border border-black my-1 justify-between bg-white'>
             {card?.image_uris?.art_crop ? <img className='h-12 w-auto rounded-tl-xl rounded-bl-xl' src={card.image_uris.art_crop} alt={card.name}/> : 
                 <img className='h-12 w-auto' src={card.card_faces[0].image_uris.art_crop} alt={card.name}/> }
             <h1 className="pr-3">{card.name}</h1>
